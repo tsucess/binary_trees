93f9c28dfa238a17a519204dbec750a6dfb0a754
tsucess <taofeeq.muhammad22@gmail.com>
2023-08-29 08:18:14 -0700

    Error fixed and updated

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -1,16 +1,16 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_inorder - pass through a binary tree using in-order traversal.
+ * binary_tree_preorder - pass through a binary tree using pre-order traversal.
  * @tree: ptr param to the root node of the tree to traverse.
  * @func: ptr to a function param to call for each node.
  */
-void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if (tree && func)
 	{
-		binary_tree_inorder(tree->left, func);
 		func(tree->n);
-		binary_tree_inorder(tree->right, func);
+		binary_tree_preorder(tree->left, func);
+		binary_tree_preorder(tree->right, func);
 	}
 }
