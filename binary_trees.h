2b44c92aa8a99bfdc7d31a882a0d07d5a5873282
Rita2024 <ritaejezie2@gmail.com>
2023-08-31 01:20:31 -0700

    binary trees

--- binary_trees.h
+++ binary_trees.h
@@ -70,6 +70,9 @@ int binary_tree_is_bst(const binary_tree_t *tree);
 bst_t *bst_insert(bst_t **tree, int value);
 bst_t *array_to_bst(int *array, size_t size);
 bst_t *bst_search(const bst_t *tree, int value);
+bst_t *bst_remove(bst_t *root, int value);
+bst_t *r_case(bst_t *node, bst_t *root);
+bst_t *r_l_case(bst_t *node, bst_t *root);
 
 
 #endif /* BINARY_TREES_H */
